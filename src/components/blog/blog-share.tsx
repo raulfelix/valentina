115c590c096298784f634e566207976bc6cb238a
raul <raul.carrizo@inquisitive.com>
2022-03-17 10:43:41 +1100

    Try chaining on window

--- src/components/blog/blog-share.tsx
+++ src/components/blog/blog-share.tsx
@@ -15,21 +15,21 @@ export const BlogShare = ({ title, url }: Props) => (
     <div>
       <Styled.BlogShareIcon
         target="_blank"
-        href={`https://twitter.com/intent/tweet?text="${title}"&url="${url}" onclick="window.open(this.href, 'twitter-share', 'width=550,height=235');return false;`}
+        href={`https://twitter.com/intent/tweet?text="${title}"&url="${url}" onclick="window?.open(this.href, 'twitter-share', 'width=550,height=235');return false;`}
       >
         <StaticImage src="../social/twitter.png" alt="Twitter" />
       </Styled.BlogShareIcon>
       <Styled.BlogShareIcon
         target="_blank"
         href={`https://www.linkedin.com/shareArticle?mini=true&url=${url}`}
-        onclick="window.open(this.href, 'width=550,height=435');return false;"
+        onclick="window?.open(this.href, 'width=550,height=435');return false;"
       >
         <StaticImage src="../social/li.png" alt="LinkedIn" />
       </Styled.BlogShareIcon>
       <Styled.BlogShareIcon
         target="_blank"
         href={`https://facebook.com/sharer.php?u=${url}`}
-        onclick="window.open(this.href, 'facebook-share', 'width=550,height=435');return false;"
+        onclick="window?.open(this.href, 'facebook-share', 'width=550,height=435');return false;"
       >
         <StaticImage src="../social/facebook.png" alt="Facebook" />
       </Styled.BlogShareIcon>
