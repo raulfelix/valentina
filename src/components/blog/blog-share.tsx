49cc3ab90a5284b7608f5b29d79f66759b9c8b72
raul <raul.carrizo@inquisitive.com>
2022-03-17 11:43:57 +1100

    Check for window in share

--- src/components/blog/blog-share.tsx
+++ src/components/blog/blog-share.tsx
@@ -9,30 +9,42 @@ type Props = {
   url: string;
 };
 
-export const BlogShare = ({ title, url }: Props) => (
-  <Styled.BlogShare>
-    <Text>shAre</Text>
-    <div>
-      <Styled.BlogShareIcon
-        target="_blank"
-        href={`https://twitter.com/intent/tweet?text="${title}"&url="${url}" onclick="window?.open(this.href, 'twitter-share', 'width=550,height=235');return false;`}
-      >
-        <StaticImage src="../social/twitter.png" alt="Twitter" />
-      </Styled.BlogShareIcon>
-      <Styled.BlogShareIcon
-        target="_blank"
-        href={`https://www.linkedin.com/shareArticle?mini=true&url=${url}`}
-        onclick="window?.open(this.href, 'width=550,height=435');return false;"
-      >
-        <StaticImage src="../social/li.png" alt="LinkedIn" />
-      </Styled.BlogShareIcon>
-      <Styled.BlogShareIcon
-        target="_blank"
-        href={`https://facebook.com/sharer.php?u=${url}`}
-        onclick="window?.open(this.href, 'facebook-share', 'width=550,height=435');return false;"
-      >
-        <StaticImage src="../social/facebook.png" alt="Facebook" />
-      </Styled.BlogShareIcon>
-    </div>
-  </Styled.BlogShare>
-);
+export const BlogShare = ({ title, url }: Props) => {
+  const isBrowser = typeof window !== "undefined";
+
+  function openWindow() {
+    if (isBrowser) {
+      window.open(window.location.href, "width=550,height=435");
+      return false;
+    }
+  }
+
+  return (
+    <Styled.BlogShare>
+      <Text>shAre</Text>
+      <div>
+        <Styled.BlogShareIcon
+          target="_blank"
+          href={`https://twitter.com/intent/tweet?text="${title}"&url="${url}"`}
+          onclick={openWindow}
+        >
+          <StaticImage src="../social/twitter.png" alt="Twitter" />
+        </Styled.BlogShareIcon>
+        <Styled.BlogShareIcon
+          target="_blank"
+          href={`https://www.linkedin.com/shareArticle?mini=true&url=${url}`}
+          onclick={openWindow}
+        >
+          <StaticImage src="../social/li.png" alt="LinkedIn" />
+        </Styled.BlogShareIcon>
+        <Styled.BlogShareIcon
+          target="_blank"
+          href={`https://facebook.com/sharer.php?u=${url}`}
+          onclick={openWindow}
+        >
+          <StaticImage src="../social/facebook.png" alt="Facebook" />
+        </Styled.BlogShareIcon>
+      </div>
+    </Styled.BlogShare>
+  );
+};
