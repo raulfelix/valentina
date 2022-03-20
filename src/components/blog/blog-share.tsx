ce3b3be76446b8ca59bd6153ef22a7eb94a0a547
raul <raul.carrizo@inquisitive.com>
2022-03-20 18:44:36 +1100

    Finally make it work

--- src/components/blog/blog-share.tsx
+++ src/components/blog/blog-share.tsx
@@ -16,9 +16,9 @@ export const BlogShare = ({ title }: Props) => {
     url = window.location.href;
   }
 
-  function openWindow() {
+  function openWindow(shareUrl: string) {
     if (isBrowser) {
-      window.open(window.location.href, "width=550,height=435");
+      window.open(`${shareUrl}${window.location.href}`, "width=550,height=435");
       return false;
     }
   }
@@ -30,21 +30,21 @@ export const BlogShare = ({ title }: Props) => {
         <Styled.BlogShareIcon
           target="_blank"
           href={`https://twitter.com/intent/tweet?text=${title}&url=${url}`}
-          onClick={openWindow}
+          onClick={() => openWindow('https://twitter.com/intent/tweet?url=')}
         >
           <StaticImage src="../social/twitter.png" alt="Twitter" width={28} />
         </Styled.BlogShareIcon>
         <Styled.BlogShareIcon
           target="_blank"
           href={`https://www.linkedin.com/shareArticle?mini=true&url=${url}`}
-          onClick={openWindow}
+          onClick={() => openWindow('https://www.linkedin.com/shareArticle?mini=true&url=')}
         >
           <StaticImage src="../social/linkedin.png" alt="LinkedIn" width={28} />
         </Styled.BlogShareIcon>
         <Styled.BlogShareIcon
           target="_blank"
-          href={`https://facebook.com/sharer.php?u=${url}`}
-          onClick={openWindow}
+          href={`https://facebook.com/sharer.php`}
+          onClick={() => openWindow('https://facebook.com/sharer.php?u=')}
         >
           <StaticImage src="../social/facebook.png" alt="Facebook"  width={14} height={28}/>
         </Styled.BlogShareIcon>
