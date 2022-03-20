f10cd3cea67b7e5f2dca167c9644e47903ed3905
raul <raul.carrizo@inquisitive.com>
2022-03-20 17:22:53 +1100

    Add publications page

---
+++ src/components/publications/publications.styles.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import { grey1, grey10, grey3 } from "../../styles/colour.styles";
+
+import { baseFocusState, rem } from "../../styles/styles";
+import { fontSize14, Heading40 } from "../../styles/typography.styles";
+
+export const List = styled.div`
+  margin: auto;
+  max-width: ${rem(600)};
+`;
+
+export const PublicationGroup = styled.div`
+  margin-bottom: ${rem(100)};
+`;
+
+export const PublicationIcon = styled.div`
+  margin-bottom: ${rem(32)};
+  text-align: center;
+  img {
+    display: inline-block;
+    height: ${rem(60)};
+  }
+`;
+
+export const PublicationHeader = styled.div`
+  background-color: ${grey3};
+  padding: ${rem(20)};
+  margin-top: -40px;
+  margin-bottom: ${rem(80)};
+  text-align: center;
+
+  ${Heading40} {
+    margin-bottom: 0;
+  }
+`;
+
+export const PublicationFilters = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: ${rem(80)};
+
+  a {
+    background-color: ${grey3};
+    color: ${grey10};
+    font-size: ${fontSize14};
+    display: inline-block;
+    border-radius: 30px;
+    padding: ${rem(4)} ${rem(8)};
+    margin: 0 ${rem(8)};
+    text-decoration: none;
+
+    &:hover {
+      background-color: ${grey10};
+      color: ${grey1};
+    }
+
+    &:focus {
+      ${baseFocusState}
+    }
+  }
+`;
---
+++ src/pages/publications.tsx
@@ -0,0 +1,116 @@
+import { graphql, PageProps } from "gatsby";
+import * as React from "react";
+
+import Layout from "../components/layout";
+import { Col, Grid, Row } from "../styles/grid.styles";
+import { PublicationCard } from "../components/publications/publication-card";
+import * as Styled from "../components/publications/publications.styles";
+
+import honisoit from "../components/publications/honisoit.png";
+import deadgoodmusic from "../components/publications/deadgoodmusic.png";
+import gluestore from "../components/publications/gluestore.png";
+
+type DataProps = {
+  allMdx: {
+    nodes: {
+      id: string;
+      frontmatter: {
+        title: string;
+        subtitle: string;
+        date: string;
+        external_link: string;
+        publisher_name: string;
+        publisher_link: string;
+      };
+    }[];
+  };
+};
+
+function getIcon(name: string) {
+  switch (name) {
+    case "Honi Soit":
+      return honisoit;
+    case "Dead Good Music":
+      return deadgoodmusic;
+  }
+  return "";
+}
+
+const PublicationsPage = ({ data }: PageProps<DataProps>) => {
+  const pubGroups: { [key: string]: any } = {};
+
+  data.allMdx.nodes.forEach((p) => {
+    const name = p.frontmatter.publisher_name;
+    if (!pubGroups[name]) {
+      pubGroups[name] = [];
+    }
+    pubGroups[name].push(p);
+  });
+
+  const keys = Object.keys(pubGroups);
+  return (
+    <Layout>
+      <Grid>
+        <Row>
+          <Col>
+            <Styled.List>
+              <Styled.PublicationFilters>
+                {keys.map((key) => (
+                  <a key={key} href={`#${key}`}>
+                    {key}
+                  </a>
+                ))}
+              </Styled.PublicationFilters>
+              {keys.map((key) => (
+                <Styled.PublicationGroup key={key} id={key}>
+                  <Styled.PublicationIcon>
+                    <img src={getIcon(key)} alt={key} height={40} />
+                  </Styled.PublicationIcon>
+                  <div>
+                    {pubGroups[key].map((p) => (
+                      <PublicationCard key={p.id} frontmatter={p.frontmatter} />
+                    ))}
+                  </div>
+                </Styled.PublicationGroup>
+              ))}
+            </Styled.List>
+          </Col>
+        </Row>
+      </Grid>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query {
+    allMdx(
+      filter: { fileAbsolutePath: { regex: "/(publication)/" } }
+      sort: { order: DESC, fields: frontmatter___date }
+    ) {
+      nodes {
+        id
+        frontmatter {
+          date(formatString: "DD MMMM yyyy")
+          title
+          subtitle
+          external_link
+          publisher_name
+          publisher_link
+          hero_image {
+            childImageSharp {
+              fixed(width: 800) {
+                srcWebp
+                srcSetWebp
+                originalName
+              }
+            }
+          }
+          hero_image_credit_link
+          hero_image_credit_text
+        }
+      }
+    }
+  }
+`;
+
+export default PublicationsPage;
